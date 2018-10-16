bc336ae6ffac8d7a5fee3bd78994e49b974c163a
qrames <ramque@orange.fr>
2018-10-16 19:17:26 +0200

    the iphone overflow-x: hidden work but it's on <html>

--- blog/static/js/script.js
+++ blog/static/js/script.js
@@ -4,14 +4,10 @@ $(document).ready(function() {
         $(this).css('top', '-100vh');
     });
     $('.menu-bar').click(function(event) {
-        $('nav').css('min-width', '800px');
-        $('nav ul li').css('visibility', 'visible');
-        $('.menu-bar').css('display', 'none');
-        $('header').mousedown(function(event) {
-            $('nav').css('min-width', '400px');
-            $('nav ul li').css('visibility', 'hidden');
-            $('.menu-bar').css('display', 'block');
-        });
+        $('nav').addClass('deploy');
+    });
+    $('header').hover(function(event) {
+        $('nav').removeClass('deploy');
     });
 
 });
